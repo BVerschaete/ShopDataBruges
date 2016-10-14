df3efaa198731af69a8614fe8f2934165152d93a
BVerschaete <bastien.verschaete@gmail.com>
2016-10-14 11:59:22 +0200

    cleaned up index html and js

--- www/js/requestJsonFromServer.js
+++ www/js/requestJsonFromServer.js
@@ -1,4 +1,10 @@
 /**
  * Created by basti on 14/10/2016.
  */
-var string = "temp";
+var URI = "http://loc.geopunt.be/geolocation/location?latlon=";
+
+var coords = "51.196873,3.254300";
+
+var URL = URI + coords;
+
+var JSONObject = jQuery.getJSON(URL);
