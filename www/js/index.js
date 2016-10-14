1135baaa1c30ddb6745648271e1f2418f7d9345f
BVerschaete <bastien.verschaete@gmail.com>
2016-10-14 18:09:53 +0200

    added logo and google maps functionality

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,24 @@
 function deviceReady(){
     $("#submitShop").click(addShop);
-    initMap();
 }
 
-function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
+$(document).on("pagehide","#start",function(){
+    getLocation(function(latlong){
+        latlong = latlong.split(",");
+        var lat = latlong[0];
+        var long = latlong[1];
+        initMap(parseFloat(lat), parseFloat(long), "map_addshop");
+    });
+});
+
+function initMap(lat, long, mapId) {
+    var stad = {lat: lat, lng: long};
+    var map = new google.maps.Map(document.getElementById(mapId), {
+        zoom: 16,
+        center: stad
     });
     var marker = new google.maps.Marker({
-        position: uluru,
+        position: stad,
         map: map
     });
 }
